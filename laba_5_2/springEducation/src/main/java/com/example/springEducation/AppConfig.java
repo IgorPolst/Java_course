4a97e2f19eacb5784b5504163ec3bfe156292b3e
IgorPolst <igorpolstianoy@gmail.com>
2025-04-14 14:38:41 +0300

    Поправил работу пропертис

--- laba_5_2/springEducation/src/main/java/com/example/springEducation/AppConfig.java
+++ laba_5_2/springEducation/src/main/java/com/example/springEducation/AppConfig.java
@@ -3,12 +3,10 @@ package com.example.springEducation;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = "com.example")
-@ImportResource("classpath:applicationContext.xml")
+@ComponentScan(basePackages = "com.example.springEducation")
 @PropertySource("classpath:MathForProgramming.properties")
 @PropertySource("classpath:FunDeepLearning.properties")
 public class AppConfig {
